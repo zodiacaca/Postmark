b84e7de6d03588bec98fbc992a68f93efd58105d
zodiacaca <121059464@qq.com>
2018-04-07 16:32:36 +0800

    Revert "u"
    This reverts commit cca27819081106b5540ae4c39b0c2630047006e1.

--- content/content-active.js
+++ content/content-active.js
@@ -38,8 +38,6 @@ function checkDOM()
     addButtons();
     colorBackground();
     
-    subfolders = [];
-    
     toggle = true;
   }
   else {
@@ -140,7 +138,7 @@ function saveData(host, page, container, link, linkText) {
       page: page,
       date: getTime()
     }
-    // console.log(item);
+    console.log(item);
     chrome.storage.local.set(item);
     styleContainer(container.container);
   });
